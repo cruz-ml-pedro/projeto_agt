246253ed578e06c1668a09059cbd29c8636565e5
cruz-ml-pedro <cruz.ml.pedro@gmail.com>
2023-02-10 16:01:55 -0300

    update

--- FUNCTIONS/recibo_number_function.R
+++ FUNCTIONS/recibo_number_function.R
@@ -4,7 +4,7 @@ numero_parcelas <- function(nome, codigo){
   require("readr")
   require("tibble")
 
-parcela_inicial <- read_delim("C:/Users/marin/Downloads/Fesp_agata/numero_recibo.txt",
+parcela_inicial <- read_delim("numero_recibo.txt",
                             col_names = TRUE,
                             show_col_types = FALSE)
 
@@ -18,9 +18,9 @@ if(parcela_inicial$ano != format(Sys.Date(),"%Y")){
   
   parcela_text <- paste(parcela_inicial$numero_parcela, parcela_inicial$ano, sep="-")
   
-  write_delim(parcela_inicial,"C:/Users/marin/Downloads/Fesp_agata/numero_recibo.txt")
+  write_delim(parcela_inicial,"/numero_recibo.txt")
   
-  df_controle <- read_delim("C:/Users/marin/Downloads/Fesp_agata/controle_recibo_consultor_mes.txt",
+  df_controle <- read_delim("/controle_recibo_consultor_mes.txt",
                           col_names = TRUE,
                           show_col_types = FALSE)
  
@@ -28,7 +28,7 @@ if(parcela_inicial$ano != format(Sys.Date(),"%Y")){
   
   df_controle <- rbind(df_controle,df_recibo)
   
-  write_delim(df_controle, "C:/Users/marin/Downloads/Fesp_agata/controle_recibo_consultor_mes.txt", append = FALSE)
+  write_delim(df_controle, "/controle_recibo_consultor_mes.txt", append = FALSE)
   
   return(parcela_text)
   
@@ -38,9 +38,9 @@ if(parcela_inicial$ano != format(Sys.Date(),"%Y")){
   
   parcela_text <- paste(parcela_inicial$numero_parcela, parcela_inicial$ano, sep="-")
   
-  write_delim(parcela_inicial,"C:/Users/marin/Downloads/Fesp_agata/numero_recibo.txt")
+  write_delim(parcela_inicial,"/numero_recibo.txt")
   
-  df_controle <- read_delim("C:/Users/marin/Downloads/Fesp_agata/controle_recibo_consultor_mes.txt",
+  df_controle <- read_delim("/controle_recibo_consultor_mes.txt",
                           col_names = TRUE,
                           show_col_types = FALSE)
                           
@@ -48,7 +48,7 @@ if(parcela_inicial$ano != format(Sys.Date(),"%Y")){
   
   df_final <- rbind(df_controle,df_recibo)
   
-  write_delim(df_final,"C:/Users/marin/Downloads/Fesp_agata/controle_recibo_consultor_mes.txt", append = FALSE)
+  write_delim(df_final,"/controle_recibo_consultor_mes.txt", append = FALSE)
   
   return(parcela_text)
   
